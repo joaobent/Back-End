4649797e740378913572d567b9c6134843d88f10
joao12345678910 <joaopedro0744@hotmail.com>
2024-02-22 11:50:58 -0400

    teste

---
+++ teste_01.js
@@ -0,0 +1,57 @@
+var nome="João Pretinho";
+var idade=15;
+console.log('Óla meu nome é',nome ,'tenho', idade,'anos');
+
+console.log('------------')
+
+console.log('banana'.toUpperCase());
+console.log('Banana'.toLowerCase());
+
+console.log('------------')
+
+var nome2="Hiago Gabiel";
+var numero=19;
+console.log(typeof nome2);
+console.log(typeof numero);
+
+console.log('------------')
+
+var imc=53/(1.70*1.70);
+console.log(imc);
+
+console.log('------------')
+
+let num=15;
+let num2=20;
+let soma=num+num2
+console.log(soma)
+let menos=num-num2
+console.log(menos)
+let vezes=num*num2
+console.log(vezes)
+let divisao=num/num2
+console.log(divisao)
+
+console.log('------------')
+
+let idade2=15
+let idade3=16
+let conta=idade2>idade3
+if (conta === false);
+    console.log("A primeira idade não é maior que a segunda idade")
+
+console.log('------------')
+
+let idade4=17
+if (idade>=18)
+    console.log('Maior de idade')
+else
+    console.log('Menor de idade');
+
+console.log('------------')
+
+let number=4
+if(number%2==0)
+    console.log('É um numero par')
+else
+    console.log('É um numero impar')
